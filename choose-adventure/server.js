bb6504ebcd187be47cd14bb055f827272b6f51f5
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-02-23 20:40:49 -0500

    Integrated passport - passed admin registration/login checks on Postman

--- choose-adventure/server.js
+++ choose-adventure/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const bodyParser = require("body-parser");
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use(bodyParser.urlencoded({extended: false}));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chooseadventure');
+const db = require("./config/keys").mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+.then(() => console.log("MongoDB successfully connected!"))
+.catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+app.use('/api/users', users);
 
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
