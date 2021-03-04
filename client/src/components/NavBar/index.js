5d1d12f5c40b0b90bac84b0cfab87b53369fac24
Isaac Frishman <ifrishman89@gmail.com>
2021-03-03 21:08:26 -0600

    fix

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -67,41 +67,4 @@ const NavBar = (props) => {
   );
 };
 
-export default Navbar;
-
-// import React, {useContext} from 'react';
-// import {Link} from 'react-router-dom';
-// import './style.css';
-// import AuthService from '../../authentication/AuthService';
-// import { AuthContext } from '../../authentication/AuthContext';
-
-// function Navbar(props) {
-//   const {isAuthenticated, user, setIsAuthenticated, setUser} = useContext(AuthContext);
-
-//   const unauthenticatedNavBar = () => {
-//     return(
-//       <Link to="/login">ADMIN</Link>
-//     )
-//   }
-
-//   const authenticatedNavBar = () => {
-//     return(
-//       <Link to="/admin">ADMIN</Link>
-//     )
-//   }
-
-//   return(
-//     <nav className="navbar navbar-default navbar-dark bg-dark justify-content-center">
-//       <ul className="nav navbar-nav navbar-expand-lg">
-//       <li className="nav-item pad">
-//           <Link to="/">HOME</Link>
-//         </li>
-//         <li className="nav-item pad">
-//           { !isAuthenticated ? unauthenticatedNavBar() : authenticatedNavBar() }
-//         </li>
-//       </ul>
-//     </nav>
-//   )
-// }
-
-// export default Navbar;
+export default NavBar;
