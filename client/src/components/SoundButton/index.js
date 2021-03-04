b2ad1de08aa6c5946fcb299885af4755a30dada6
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-03-04 14:11:08 -0500

    Changed sound button text to be play/stop font awesome icons

--- client/src/components/SoundButton/index.js
+++ client/src/components/SoundButton/index.js
@@ -2,20 +2,26 @@ import React from "react";
 import Audio from "./theme.mp3";
 import { Howl, Howler } from "howler";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+import { faStopCircle } from '@fortawesome/free-solid-svg-icons';
+
+let playIcon = <FontAwesomeIcon icon={faPlayCircle} />;
+let stopIcon = <FontAwesomeIcon icon={faStopCircle} />;
 
 function SoundButton() {
   const [playing, setPlaying] = React.useState(false);
-  const [buttonText, setButtonText] = React.useState("PLAY!");
+  const [buttonText, setButtonText] = React.useState(playIcon);
 
   var sound = new Howl({
     src: [Audio],
     onplay: function() {
       setPlaying(true);
-      setButtonText("STOP!");
+      setButtonText(stopIcon);
     },
     onstop: function() {
       setPlaying(false);
-      setButtonText("PLAY!");
+      setButtonText(playIcon);
     }
   });
 
